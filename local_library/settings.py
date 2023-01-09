87cba1f1d25fcf7b1e3c3f54438b6ffd97140077
Michael Souza <michael@ufc.br>
2023-01-09 15:54:16 -0300

    Bugfix static files

--- local_library/settings.py
+++ local_library/settings.py
@@ -140,7 +140,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = 'static/'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
