0972ffabd780f78a94bfffff8edc2d384a036232
Michael Souza <michael@ufc.br>
2023-01-09 15:09:40 -0300

    Add whitenoise

--- local_library/settings.py
+++ local_library/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'catalog.apps.CatalogConfig',
 ]
 
