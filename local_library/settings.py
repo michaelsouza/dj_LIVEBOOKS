06725486bfb21b899ec32a086b3d388923d843d7
Michael Souza <michael@ufc.br>
2023-01-09 14:11:32 -0300

    Change CSRF_TRUSTED_ORIGINS

--- local_library/settings.py
+++ local_library/settings.py
@@ -159,7 +159,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-local-library.up.railway.app'
+]
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
